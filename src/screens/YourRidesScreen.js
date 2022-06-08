036eeef7c02f53e80e548743fb8f1a8382d6158a
JIshanRandika <jishanrandika@gmail.com>
2022-06-08 22:04:21 +0530

    your rides screen

---
+++ src/screens/YourRidesScreen.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import {ActivityIndicator, Button, StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
+
+function YourRidesScreen() {
+    const {userInfo, isLoading, logout} = useContext(AuthContext);
+
+    return (
+        <View style={styles.container}>
+            {/*<Spinner visible={isLoading} />*/}
+            <Text style={styles.welcome}>Your Rides Screen</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    welcome: {
+        fontSize: 18,
+        marginBottom: 0,
+    },
+});
+
+export default YourRidesScreen;
