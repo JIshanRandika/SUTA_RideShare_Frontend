ef24f39ba97b5f7349747324fe344e2e8e7f7d19
JIshanRandika <jishanrandika@gmail.com>
2022-06-08 22:02:12 +0530

    app navigation

---
+++ src/components/AppNavigation.js
@@ -0,0 +1,69 @@
+import React, {useContext, useEffect, useState} from 'react';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+
+import RiderScreen from '../screens/RiderScreen';
+import DriverScreen from '../screens/DriverScreen';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+import YourRidesScreen from '../screens/YourRidesScreen';
+import AvailableRidesScreen from '../screens/AvailableRidesScreen';
+import YourDrivesScreen from '../screens/YourDrivesScreen';
+import AvailableVehiclesScreen from '../screens/AvailableVehiclesScreen';
+import AddADriveScreen from '../screens/AddADriveScreen';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const DriverStack = createNativeStackNavigator();
+
+function DriverStackScreen() {
+    return (
+        <DriverStack.Navigator>
+            <DriverStack.Screen name="Driver" component={DriverScreen} options={{headerShown: false}}/>
+            <DriverStack.Screen name="Your Drives" component={YourDrivesScreen} />
+            <DriverStack.Screen name="Available Rides" component={AvailableRidesScreen} />
+            <DriverStack.Screen name="Add a Drive" component={AddADriveScreen} />
+        </DriverStack.Navigator>
+    );
+}
+
+
+
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+            <HomeStack.Screen name="Details" component={DetailsScreen} />
+        </HomeStack.Navigator>
+    );
+}
+
+
+const RiderStack = createNativeStackNavigator();
+
+function RiderStackScreen() {
+    return (
+        <RiderStack.Navigator>
+            <RiderStack.Screen name="Rider" component={RiderScreen} options={{headerShown: false}}/>
+            <RiderStack.Screen name="Available Vehicles" component={AvailableVehiclesScreen} />
+            <RiderStack.Screen name="Your Rides" component={YourRidesScreen} />
+        </RiderStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigation() {
+    return (
+            <Tab.Navigator>
+                <Tab.Screen name="Rider" component={RiderStackScreen} />
+                <Tab.Screen name="Home" component={HomeStackScreen} />
+                <Tab.Screen name="Driver" component={DriverStackScreen} />
+            </Tab.Navigator>
+    );
+}
+
