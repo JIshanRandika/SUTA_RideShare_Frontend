d4ae395bc06d1b139fd381c26417bbb68e538c44
JIshanRandika <jishanrandika@gmail.com>
2022-06-08 22:02:30 +0530

    available rider screen

---
+++ src/screens/AvailableRidesScreen.js
@@ -0,0 +1,29 @@
+import React, {useContext} from 'react';
+import {ActivityIndicator, Button, StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
+
+function AvailableRidesScreen() {
+    const {userInfo, isLoading, logout} = useContext(AuthContext);
+
+    return (
+        <View style={styles.container}>
+            {/*<Spinner visible={isLoading} />*/}
+            <Text style={styles.welcome}>Available Rides Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    welcome: {
+        fontSize: 18,
+        marginBottom: 0,
+    },
+});
+
+export default AvailableRidesScreen;
