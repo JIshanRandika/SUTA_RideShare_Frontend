21fea70fbfc7551016ffd67711af8680cc93fe02
JIshanRandika <jishanrandika@gmail.com>
2022-06-10 13:12:07 +0530

    your driver screen update

--- src/screens/DriverScreen.js
+++ src/screens/DriverScreen.js
@@ -69,6 +69,38 @@ function DriverScreen({ navigation }) {
                 <Text style={{fontSize: 22, fontWeight:"bold", textAlign:"center",color:"#ffffff"}}>Available Rides</Text>
             </TouchableOpacity>
             </View>
+                <View style={{flex:1, width:'100%'}}>
+                        <TouchableOpacity
+                            style={{
+                                    height:'80%',
+                                    marginTop:"3%",
+                                    alignSelf: 'center',
+                                    alignItems: 'center',
+                                    justifyContent:'center',
+                                    width: "90%",
+                                    // height: 37,
+                                    paddingLeft:10,
+                                    paddingRight:10,
+                                    paddingTop:10,
+                                    paddingBottom:10,
+                                    backgroundColor: "#114953",
+
+                                    borderRadius:10,
+                                    shadowColor: "#0090ff",
+                                    shadowOffset: {
+                                            width: 0,
+                                            height: 5,
+                                    },
+                                    shadowOpacity: 0.34,
+                                    shadowRadius: 6.27,
+
+                                    elevation: 10,
+                            }}
+                            onPress={() => navigation.navigate('Your Request To Riders')}
+                        >
+                                <Text style={{fontSize: 22, fontWeight:"bold", textAlign:"center",color:"#ffffff"}}>Your Request To Riders</Text>
+                        </TouchableOpacity>
+                </View>
 
         </View>
     );
