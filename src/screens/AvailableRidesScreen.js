f323a7f616c901e56046cb84f86e9f8210425806
JIshanRandika <jishanrandika@gmail.com>
2022-06-10 13:13:00 +0530

    your available rides screen update

--- src/screens/AvailableRidesScreen.js
+++ src/screens/AvailableRidesScreen.js
@@ -1,28 +1,306 @@
-import React, {useContext} from 'react';
-import {ActivityIndicator, Button, StyleSheet, Text, View} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+    ActivityIndicator,
+    Button,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    Text, TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 import {AuthContext} from '../context/AuthContext';
+import {BASE_URL} from '../config';
+import MapView, {Circle, Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
-function AvailableRidesScreen() {
-    const {userInfo, isLoading, logout} = useContext(AuthContext);
+var FCM = require('fcm-node');
 
+
+function AvailableRidesScreen({ navigation }) {
+    const {userInfo, logout} = useContext(AuthContext);
+
+    const [isLoading, setLoading] = useState(true);
+
+    const [data, setData] = useState([]);
+    console.log(data);
+
+
+    useEffect(() => {
+        fetch(`${BASE_URL}/getRides`)
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+
+
+    }, []);
+
+    // useEffect(() => {
+    //     // declare the async data fetching function
+    //     const fetchData = async () => {
+    //         // get the data from the api
+    //         const data = await fetch(`${BASE_URL}/getDrives`);
+    //         // convert the data to json
+    //         const json = await data.json();
+    //         // set state with the result
+    //         setDrivesData(json);
+    //         // console.log(json)
+    //     }
+    //
+    //     // call the function
+    //     fetchData()
+    //         // make sure to catch any error
+    //         .catch(console.error);
+    //
+    //     setInterval(function(){
+    //         console.log(drivesData)
+    //     },3000);
+    //
+    //     console.log('asd')
+    // },[]);
+
+    const addARide = (navigation) => {
+        fetch(`${BASE_URL}/addADriverToRiderRequest`, {
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                riderEmail: userInfo.email,
+                riderName: userInfo.name,
+                driverEmail: itemEmail,
+                driverName: driverName,
+                originDateTime: itemOriginDateTime,
+                status: 'Pending Request'
+
+            }),
+        });
+
+
+        fetch(`${BASE_URL}/notification/send`, {
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                to: userInfo.deviseToken,
+                notificationTitle: 'Send Request',
+                notificationBody: 'Body of your push notification',
+
+            }),
+        });
+
+        // if(contactNumber.length>0){
+        //     alert('Successfully Completed')
+        // }else {
+        //     alert('Error')
+        // }
+
+
+    }
+
+    const [originLongitude,setOriginLongitude] = useState(6.586622);
+    const [originLatitude,setOriginLatitude] = useState(6.586622);
+    const [destinationLongitude,setDestinationLongitude] = useState(6.586622);
+    const [destinationLatitude,setDestinationLatitude] = useState(6.586622);
+
+    const [driverName, setDriverName] = useState('null');
+    const [vehicleNumber,setVehicleNumber] = useState('null')
+
+    const [itemEmail, setItemEmail] = useState('email');
+    const [itemOriginDateTime, setItemOriginDateTime] = useState('null')
+
+    const [screen,setScreen] = useState('1');
+
+    const [neededSeats, setNeededSeats] = useState(0)
+
+    const [selectedId, setSelectedId] = useState(null);
+    const Item = ({ item }) => (
+
+
+
+        <TouchableOpacity
+            onPress={()=>{{
+                setOriginLatitude(item.originLatitude);
+                setOriginLongitude(item.originLongitude);
+                setDestinationLatitude(item.destinationLatitude);
+                setDestinationLongitude(item.destinationLongitude);
+                setItemEmail(item.email);
+                setItemOriginDateTime(item.originDateTime);
+                setScreen('2');
+                setDriverName(item.username);
+                setVehicleNumber(item.VehicleNumber);
+            }}
+            }
+            style={{
+                // flex: 1,
+                marginTop:"3%",
+                alignSelf: 'center',
+                width: "90%",
+                // height: 37,
+                paddingLeft:10,
+                paddingRight:10,
+                paddingTop:10,
+                paddingBottom:10,
+                backgroundColor: "#05b7e3",
+
+                borderRadius:10,
+                shadowColor: "#0090ff",
+                shadowOffset: {
+                    width: 0,
+                    height: 5,
+                },
+                shadowOpacity: 0.34,
+                shadowRadius: 6.27,
+
+                elevation: 10,
+            }}
+
+        >
+
+
+            {/*<Text style={{fontSize: 15, fontWeight:"bold", textAlign:"center",color:"#ffffff"}}>{item.originDateTime}</Text>*/}
+            <Text style={{fontSize: 15, fontWeight:"bold", textAlign:"left",color:"#ffffff"}}>Name: {item.username}</Text>
+            <Text style={{fontSize: 15, fontWeight:"bold", textAlign:"left",color:"#ffffff"}}>Needed Seats: {item.neededSeats}</Text>
+            <Text style={{fontSize: 15, fontWeight:"bold", textAlign:"left",color:"#ffffff"}}>Contact Number: {item.contactNumber}</Text>
+            <Text style={{fontSize: 15, fontWeight:"bold", textAlign:"left",color:"#ffffff"}}>{item.originDateTime}</Text>
+            <Text style={{fontSize: 15, fontWeight:"bold", textAlign:"left",color:"#ffffff"}}>{item.destinationDateTime}</Text>
+
+
+        </TouchableOpacity>
+    );
+    const renderItem = ({ item }) => {
+        return (
+            <Item
+                item={item}
+            />
+        );
+    };
     return (
-        <View style={styles.container}>
+
+        <View style={{
+            flex: 1,
+            // alignItems: 'center',
+            // justifyContent: 'center',
+            // width:'100%'
+        }}>
+
             {/*<Spinner visible={isLoading} />*/}
-            <Text style={styles.welcome}>Available Rides Screen</Text>
+            {/*<Text style={styles.welcome}>Your Drives Screen</Text>*/}
+            {screen === '1' && (
+                <>
+
+
+                    <View style={styles.container}>
+                        {/*<Spinner visible={isLoading} />*/}
+
+
+                        {isLoading ? <Text>Loading...</Text> :(
+                            <View style={{
+                                flex: 12,
+                                height:'100%',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                width:'100%'
+                            }}>
+                                {/*<Text style={{justifyContent:'center'}}>Loaded</Text>*/}
+                                <View style={{width:'100%'}}>
+                                    <SafeAreaView style={{width:'100%'}}>
+
+                                        <FlatList
+                                            style={{height:"100%", width:'100%'}}
+                                            data={data}
+                                            renderItem={renderItem}
+                                            keyExtractor={(data) => data._id}
+                                            extraData={selectedId}
+                                        />
+
+                                    </SafeAreaView>
+                                </View>
+                            </View>
+                        )}
+
+                    </View>
+                </>
+            )}
+            {screen === '2' && (
+                <>
+
+                    <MapView
+                        // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                        style={styles.map}
+
+                        initialRegion={{
+                            latitude: originLatitude,
+                            longitude: originLongitude,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+
+                    >
+
+
+
+                        <Marker coordinate={{latitude: originLatitude, longitude: originLongitude}}/>
+
+                        <Circle center={{
+                            latitude: originLatitude,
+                            longitude: originLongitude,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421}} radius={500} />
+
+                        <Marker coordinate={{latitude: destinationLatitude, longitude: destinationLongitude}}/>
+                        <Circle center={{
+                            latitude: destinationLatitude,
+                            longitude: destinationLongitude,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421}} radius={500} />
+                        <MapViewDirections
+                            origin={{latitude: originLatitude, longitude: originLongitude}}
+                            destination={{latitude: destinationLatitude, longitude: destinationLongitude}}
+                            apikey={'AIzaSyCT1sEzJHHoRDcScafHAebRp7tP_ZYc6p8'}
+                            strokeWidth={3}
+                            strokeColor="hotpink"
+                        />
+
+
+                    </MapView>
+                    <View style={{flex: 2,margin:10}}>
+                        <Button title='Back' onPress={()=>{setScreen('1')}}/>
+                    </View>
+                    {/*<TextInput*/}
+                    {/*    style={{margin:10,backgroundColor:'yellow'}}*/}
+                    {/*    value={neededSeats}*/}
+                    {/*    placeholder="Number of Seats"*/}
+                    {/*    onChangeText={text => setNeededSeats(text)}*/}
+                    {/*/>*/}
+                    <View style={{margin:10}}>
+                        <Button color={'green'} title='Send the Request' onPress={()=>{navigation.navigate('Driver');addARide();}}/>
+                    </View>
+                </>
+            )}
         </View>
     );
-}
 
-
+};
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        height:'100%',
         alignItems: 'center',
         justifyContent: 'center',
     },
-
+    map: {
+        // height:'50%',
+        ...StyleSheet.absoluteFillObject,
+    },
     welcome: {
         fontSize: 18,
         marginBottom: 0,
