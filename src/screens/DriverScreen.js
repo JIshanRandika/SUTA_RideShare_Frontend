4b6692ab7eec2c80895265b96c89673ae87757f8
JIshanRandika <jishanrandika@gmail.com>
2022-06-10 11:13:01 +0530

    drive screen ui update

--- src/screens/DriverScreen.js
+++ src/screens/DriverScreen.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import {ActivityIndicator, Button, Text, View} from 'react-native';
+import {ActivityIndicator, Button, Text, View, TouchableOpacity} from 'react-native';
 
 function DriverScreen({ navigation }) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Driver screen</Text>
-            <Button
-                color='green'
-                title="Your Drives"
+            {/*<Text>Driver screen</Text>*/}
+            <View style={{flex:1, width:'100%'}}>
+            <TouchableOpacity
+                style={{
+                    height:'80%',
+                    marginTop:"3%",
+                    alignSelf: 'center',
+                    alignItems: 'center',
+                    justifyContent:'center',
+                    width: "90%",
+                    // height: 37,
+                    paddingLeft:10,
+                    paddingRight:10,
+                    paddingTop:10,
+                    paddingBottom:10,
+                    backgroundColor: "#137801",
+
+                    borderRadius:10,
+                    shadowColor: "#0090ff",
+                    shadowOffset: {
+                        width: 0,
+                        height: 5,
+                    },
+                    shadowOpacity: 0.34,
+                    shadowRadius: 6.27,
+
+                    elevation: 10,
+                }}
                 onPress={() => navigation.navigate('Your Drives')}
-            />
-            <Button
-                title="Available Rides"
+            >
+                <Text style={{fontSize: 22, fontWeight:"bold", textAlign:"center",color:"#ffffff"}}>Your Drives</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={{flex:1, width:'100%'}}>
+            <TouchableOpacity
+                style={{
+                    height:'80%',
+                    marginTop:"3%",
+                    alignSelf: 'center',
+                    alignItems: 'center',
+                    justifyContent:'center',
+                    width: "90%",
+                    // height: 37,
+                    paddingLeft:10,
+                    paddingRight:10,
+                    paddingTop:10,
+                    paddingBottom:10,
+                    backgroundColor: "#2b1153",
+
+                    borderRadius:10,
+                    shadowColor: "#0090ff",
+                    shadowOffset: {
+                        width: 0,
+                        height: 5,
+                    },
+                    shadowOpacity: 0.34,
+                    shadowRadius: 6.27,
+
+                    elevation: 10,
+                }}
                 onPress={() => navigation.navigate('Available Rides')}
-            />
+            >
+                <Text style={{fontSize: 22, fontWeight:"bold", textAlign:"center",color:"#ffffff"}}>Available Rides</Text>
+            </TouchableOpacity>
+            </View>
 
         </View>
     );
