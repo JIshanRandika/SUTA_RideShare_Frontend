b05910900713b337927369b5dde9bdeede56fdbb
JIshanRandika <jishanrandika@gmail.com>
2022-06-11 15:29:14 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://192.168.8.101:8080/api';
-export const BASE_URL = 'https://sutarideshare.herokuapp.com/api';
+export const BASE_URL = 'http://192.168.8.101:8080/api';
+// export const BASE_URL = 'https://sutarideshare.herokuapp.com/api';
